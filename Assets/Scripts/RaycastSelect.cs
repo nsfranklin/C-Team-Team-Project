5a162ebd1bd5bac65fb3ceae83f4010a411f08a0
nsfranklin <nickfranklin@rocketmail.com>
2018-12-13 01:50:51 +0000

    Created RaycastSelect Script
    kinda works but not really selecting the floor moves scene

--- Assets/Scripts/RaycastSelect.cs
+++ Assets/Scripts/RaycastSelect.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class RaycastScript : MonoBehaviour
+public class RaycastSelect : MonoBehaviour
 {
+    public string SceneToLoad;
 
-    // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        float theDistance = -1.0f;
+        float theDistance;
 
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
         Debug.DrawRay(transform.position, forward, Color.green);
 
-        if (Physics.Raycast(transform.position, (forward), out hit))
+        if (Physics.Raycast(transform.position, (forward), out hit) && OVRInput.Get(OVRInput.Button.One))
         {
+            print("Scene Selected");
             theDistance = hit.distance;
-            print(theDistance + " " + hit.collider.gameObject.name);
+            SceneManager.LoadScene(SceneToLoad);
         }
 
     }
