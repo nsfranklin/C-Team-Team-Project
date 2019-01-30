6c585770568e894ec89091089714204640b369c1
nsfranklin <nickfranklin@rocketmail.com>
2019-01-30 13:57:34 +0000

    Added NuGet Asset mangement Plugin

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MySql.Data.MySqlClient;
+using MySql;
 
 public class GameManager : MonoBehaviour {
 
@@ -18,7 +20,23 @@ public class GameManager : MonoBehaviour {
         }
 
         DontDestroyOnLoad(gameObject);
-    }
+
+        string connStr = "server=cteamteamprojectdatabase.csed5aholavi.eu-west-2.rds.amazonaws.com;user=vruser;database=world;port=3306;password=9ZxgnmXHSIdYIsK5qoGm";
+        MySqlConnection conn = new MySqlConnection(connStr);
+        try
+        {
+            Debug.Log("Connecting to MySQL...");
+            conn.Open();
+            // Perform database operations
+        }
+        catch (MySqlException ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+        conn.Close();
+        Debug.Log("Done.");
+    
+}
 	
 	// Update is called once per frame
 	void Update () {
